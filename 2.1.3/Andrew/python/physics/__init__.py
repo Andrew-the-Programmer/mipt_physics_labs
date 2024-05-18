d85771506564c4c8c6d8dcd627743464cb76951a
Andrew Chikin <chikin.ap@phystech.edu>
2024-05-18 18:43:03 +0300

    delete 2.1.3

--- 2.1.3/Andrew/python/physics/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ['calculate', 'ipython_physics', 'my_time', 'PQ', 'VNE']
-from .calculate import *
-from .ipython_physics import *
-# from .my_physics import *
-from .my_time import *
-from .PQ import *
-from .VNE import *
